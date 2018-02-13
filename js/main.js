8d99d9ca0c688c1fca1c76b46e21259b62cbf114
Isaac Seo <iseo12@gmail.com>
2018-02-12 20:09:42 -0800

    onclick function added

---
+++ js/main.js
@@ -0,0 +1,5 @@
+function btnClick() {
+  var inputValue = document.getElementById('input-bar').value;
+  console.log(inputValue);
+  document.getElementById('input-value').innerHTML = inputValue;
+}
